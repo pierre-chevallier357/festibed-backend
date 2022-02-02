aa32a783606a464138e90de7a43ecd7d72884adc
Loris Guerrin <loris.guerrin@etu.univ-grenoble-alpes.fr>
2022-02-02 21:48:43 +0100

    back to old version

--- src/main/java/com/example/RestServer.java
+++ src/main/java/com/example/RestServer.java
@@ -1,16 +1,12 @@
 
 package com.example;
-import java.util.Collections;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RestServer {
     public static void main(String[] args) {
-        SpringApplication app = new SpringApplication(RestServer.class);
-        app.setDefaultProperties(Collections.singletonMap("server.port", "4200"));
-        app.run(args);
+        SpringApplication.run(RestServer.class, args);
     }
 
 }
