e6e8498cbd5f2b1c7f00ba7f9245ac333bed89df
Loris Guerrin <loris.guerrin@etu.univ-grenoble-alpes.fr>
2022-02-03 11:10:54 +0100

    remove warning import

--- src/main/java/com/example/RestServer.java
+++ src/main/java/com/example/RestServer.java
@@ -4,9 +4,6 @@ import java.util.Collections;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 public class RestServer {
