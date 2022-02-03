97b437d82e2bfa9f49188d30486d593cb6646dfb
Loris Guerrin <loris.guerrin@etu.univ-grenoble-alpes.fr>
2022-02-03 08:59:58 +0100

    change port

--- src/main/java/com/example/RestServer.java
+++ src/main/java/com/example/RestServer.java
@@ -1,12 +1,16 @@
 
 package com.example;
+import java.util.Collections;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RestServer {
     public static void main(String[] args) {
-        SpringApplication.run(RestServer.class, args);
+        SpringApplication app = new SpringApplication(RestServer.class);
+        app.setDefaultProperties(Collections.singletonMap("server.port", "4200"));
+        app.run(args);
     }
 
 }
