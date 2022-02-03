080c362c6bc93d7f9998658febda7964af46774f
Loris Guerrin <loris.guerrin@etu.univ-grenoble-alpes.fr>
2022-02-03 09:52:15 +0100

    change restServer

--- src/main/java/com/example/RestServer.java
+++ src/main/java/com/example/RestServer.java
@@ -4,9 +4,19 @@ import java.util.Collections;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 public class RestServer {
+
+    @RequestMapping(
+        value = "/hello", 
+        method = RequestMethod.GET,
+        produces = "application/json")
+    @ResponseBody
+
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(RestServer.class);
         app.setDefaultProperties(Collections.singletonMap("server.port", "4200"));
